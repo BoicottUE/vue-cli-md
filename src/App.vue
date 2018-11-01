eeec59e74cd4fd5055175a8f7aec05a7d3eef5b4
Fer Gonz <fer@boicott.eu>
2018-11-01 14:21:34 +0100

    First build with <3

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">VUE-CLI + MATERIAL DESIGN </span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
@@ -14,7 +13,6 @@
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-toolbar>
-
     <v-content>
       <HelloWorld/>
     </v-content>
